2b2c9921fd8dc5f2623d9ace2531f119d333b2ae
Michael Lindon <michael.s.lindon@gmail.com>
2014-02-15 18:57:03 -0500

    Generating xa by lambda shift method

--- normal.cpp
+++ normal.cpp
@@ -1,21 +1,25 @@
 #include <cstdlib>
 #include <iostream>
-#include <vector>
-#include <new>
 #include <armadillo>
 
 using namespace std;
 using namespace arma;
 
-extern "C" void normal(double *ryo, double *rxo, int *rno, int *rp){
+extern "C" void normal(double *ryo, double *rxo, int *rno, int *rp, int *niter){
 
-//	mat xa(*rp,*rp);
+	mat xa(*rp,*rp);
+	mat xaxa(*rp,*rp);
+	mat xo(*rno,*rp);
 	colvec yo(*rno);
-//	colvec ya(*rp);
-
+	vec gamma(*rp,fill::ones);
 
  std::copy(ryo, ryo + *rno, &yo(0));
-//	cout << yo << endl;
-	cout << yo << endl;
-	cout << yo(2) << endl;
+ std::copy(rxo, rxo+(*rno * *rp), &xo(0,0));
+
+ xaxa=xo.t()*xo;
+ xaxa.diag()=vec(xaxa.n_rows,fill::zeros);
+ vec xaxaeigval;
+ eig_sym(xaxaeigval,xaxa);
+ xaxa-=(xaxaeigval(0)-1)*eye(xaxa.n_rows,xaxa.n_cols);
+ xa=chol(xaxa);
 }
