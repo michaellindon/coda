de6ff0d644874778139fe172846f674a6b730343
Michael Lindon <michael.s.lindon@gmail.com>
2014-02-14 20:40:59 -0500

    Achieved armadillo functionality but requires setting export PKG_LIBS="-larmadillo" and export PKG_CXXFLAGS="-larmadillo"

--- normal.cpp
+++ normal.cpp
@@ -2,9 +2,14 @@
 #include <iostream>
 #include <vector>
 #include <new>
+#include <armadillo>
 
 using namespace std;
+using namespace arma;
 
 extern "C" void normal(){
 	std::cout << "Printing to R test" << endl;
+	  mat A = randu<mat>(4,5);
+	    mat B = randu<mat>(4,5);
+	      cout << A*B.t() << endl;
 }
