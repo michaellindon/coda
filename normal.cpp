d8ed7d0b408f4869ecc51bdf3da001713703d897
Michael Lindon <michael.s.lindon@gmail.com>
2014-02-14 23:22:44 -0500

    Copied the data into a matrix classes

--- normal.cpp
+++ normal.cpp
@@ -7,9 +7,15 @@
 using namespace std;
 using namespace arma;
 
-extern "C" void normal(){
-	std::cout << "Printing to R test" << endl;
-	  mat A = randu<mat>(4,5);
-	    mat B = randu<mat>(4,5);
-	      cout << A*B.t() << endl;
+extern "C" void normal(double *ryo, double *rxo, int *rno, int *rp){
+
+//	mat xa(*rp,*rp);
+	colvec yo(*rno);
+//	colvec ya(*rp);
+
+
+ std::copy(ryo, ryo + *rno, &yo(0));
+//	cout << yo << endl;
+	cout << yo << endl;
+	cout << yo(2) << endl;
 }
