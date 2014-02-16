9f42c09f258aa1358b72da4f7d44d553c81e35eb
Michael Lindon <michael.s.lindon@gmail.com>
2014-02-15 19:45:31 -0500

    Commit progress

--- normal.cpp
+++ normal.cpp
@@ -5,21 +5,42 @@
 using namespace std;
 using namespace arma;
 
-extern "C" void normal(double *ryo, double *rxo, int *rno, int *rp, int *niter){
+extern "C" void normal(double *ryo, double *rxo, int *rno, int *rp, int *rniter){
 
-	mat xa(*rp,*rp);
-	mat xaxa(*rp,*rp);
-	mat xo(*rno,*rp);
-	colvec yo(*rno);
-	vec gamma(*rp,fill::ones);
+	int niter=*rniter;
+	int p=*rp;
+	int no=*rno;
 
- std::copy(ryo, ryo + *rno, &yo(0));
- std::copy(rxo, rxo+(*rno * *rp), &xo(0,0));
+	mat xa(p,p);
+	mat xaxa(p,p);
+	mat xo(no,p);
+	colvec yo(no);
+	ivec gamma(p,fill::ones);
+	vec xaxaeigval;
 
- xaxa=xo.t()*xo;
- xaxa.diag()=vec(xaxa.n_rows,fill::zeros);
- vec xaxaeigval;
- eig_sym(xaxaeigval,xaxa);
- xaxa-=(xaxaeigval(0)-1)*eye(xaxa.n_rows,xaxa.n_cols);
- xa=chol(xaxa);
+	std::copy(ryo, ryo + yo.n_elem, &yo(0));
+	std::copy(rxo, rxo + xo.n_elem, &xo(0,0));
+
+	xaxa=xo.t()*xo;
+	xaxa.diag()=vec(p,fill::zeros);
+	eig_sym(xaxaeigval,xaxa);
+	xaxa-=(xaxaeigval(0)-1)*eye(xaxa.n_rows,xaxa.n_cols);
+	xa=chol(xaxa);
+
+
+
+	colvec phi_mcmc(niter,fill::ones);
+	mat ya_mcmc(niter,p,fill::zeros);
+	mat gamma_mcmc(niter,p,fill::ones);
+
+	int a=(no-1)/2;
+	int b;
+
+	for (int t = 0; t < niter; t++)
+	{
+		/*Run Gibbs Sampler*/
+
+
+	}
+
 }
