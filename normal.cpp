31cc14dd7f90954ecda8fead4810d4105990591d
Michael Lindon <michael.s.lindon@gmail.com>
2014-02-15 20:19:49 -0500

    Xag achieved

--- normal.cpp
+++ normal.cpp
@@ -5,33 +5,40 @@
 using namespace std;
 using namespace arma;
 
-extern "C" void normal(double *ryo, double *rxo, int *rno, int *rp, int *rniter){
+extern "C" void normal(double *ryo, double *rxo, int *rno, int *rp, double *rlam, int *rniter){
 
 	int niter=*rniter;
 	int p=*rp;
 	int no=*rno;
 
-	mat xa(p,p);
-	mat xaxa(p,p);
-	mat xo(no,p);
-	colvec yo(no);
-	ivec gamma(p,fill::ones);
-	vec xaxaeigval;
+	Mat<double> xa(p,p);
+	Mat<double> xagam;
+	Mat<double> xaxa(p,p);
+	Mat<double> Lam(p,p);
+	Mat<double> xo(no,p);
+	Col<double> yo(no);
+	Col<uword> gamma(p,fill::ones);
+	Col<uword> inc_indices(p,fill::ones);
+	Col<double> xaxa_eigenval(p);
+	Col<double> lam(p);
 
-	std::copy(ryo, ryo + yo.n_elem, &yo(0));
-	std::copy(rxo, rxo + xo.n_elem, &xo(0,0));
+	std::copy(ryo, ryo + yo.n_elem, yo.memptr());
+	std::copy(rxo, rxo + xo.n_elem, xo.memptr());
+	std::copy(rlam, rlam + lam.n_elem, lam.memptr());
+	Lam=diagmat(lam);
+
 
 	xaxa=xo.t()*xo;
 	xaxa.diag()=vec(p,fill::zeros);
-	eig_sym(xaxaeigval,xaxa);
-	xaxa-=(xaxaeigval(0)-1)*eye(xaxa.n_rows,xaxa.n_cols);
+	eig_sym(xaxa_eigenval,xaxa);
+	xaxa-=(xaxa_eigenval(0)-1)*eye(xaxa.n_rows,xaxa.n_cols);
 	xa=chol(xaxa);
 
 
 
-	colvec phi_mcmc(niter,fill::ones);
-	mat ya_mcmc(niter,p,fill::zeros);
-	mat gamma_mcmc(niter,p,fill::ones);
+	Col<double> phi_mcmc(niter,fill::ones);
+	Mat<double> ya_mcmc(niter,p,fill::zeros);
+	Mat<double> gamma_mcmc(niter,p,fill::ones);
 
 	int a=(no-1)/2;
 	int b;
@@ -39,8 +46,8 @@ extern "C" void normal(double *ryo, double *rxo, int *rno, int *rp, int *rniter)
 	for (int t = 0; t < niter; t++)
 	{
 		/*Run Gibbs Sampler*/
-
-
+		inc_indices=find(gamma);
+		xagam=xa.cols(inc_indices);
 	}
 
 }
