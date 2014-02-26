1c4a828de6abae658e8bb22b0eb4181e1f3e93c7
Michael Lindon <michael.s.lindon@gmail.com>
2014-02-25 21:49:54 -0500

    Added some other tests

--- run.R
+++ run.R
@@ -4,34 +4,43 @@ dyn.load("normal.so")
 set.seed(1)
 no=200
 foo=rnorm(no,0,1)
-sd=3
-xo=cbind(foo+rnorm(no,0,sd),foo+rnorm(no,0,sd),foo+rnorm(no,0,sd),foo+rnorm(no,0,sd),foo+rnorm(no,0,sd),foo+rnorm(no,0,sd),foo+rnorm(no,0,sd))
-b=c(1,0,1,0,1,1,0)
+sd=10
+xo=cbind(foo+rnorm(no,0,sd),foo+rnorm(no,0,sd),foo+rnorm(no,0,sd),foo+rnorm(no,0,sd),foo+rnorm(no,0,sd),foo+rnorm(no,0,sd),foo+rnorm(no,0,sd),foo+rnorm(no,0,sd))
+b=rep(0,8);
+b[1]=1;
+b[5]=1;
 xo%*%b
-yo=xo%*%b+rnorm(no,0,1)+10000
+yo=xo%*%b+rnorm(no,0,3)+10000
 p=length(b)
-niter=10000
+niter=20000
 lam=rep(1,p)
 priorprob=rep(0.5,p)
 incprob=matrix(0,p,niter);
+gamma=matrix(0,p,niter);
 ya=matrix(0,p,niter);
 phi=rep(0,niter)
+xa=matrix(0,p,p);
+scale=rep(0,p);
 
 #C++oda
 Sys.time()->start;
-res=.C("normal",as.double(yo),as.double(xo),as.integer(no),as.integer(p),as.double(lam),as.integer(niter),as.double(priorprob),as.double(incprob),as.double(phi),as.double(ya))
+res=.C("normal",as.double(yo),as.double(xo),as.integer(no),as.integer(p),as.double(lam),as.integer(niter),as.double(priorprob),as.double(incprob),as.double(phi),as.double(ya),as.double(xa),as.double(scale),as.integer(gamma))
 print(Sys.time()-start);
 mean(res[[9]])
-incprob=as.vector(res[[8]])
-incprob=matrix(incprob,p,niter)
-incprob=apply(incprob,1,mean)
+prob_mcmc=as.vector(res[[8]])
+prob_mcmc=matrix(prob_mcmc,p,niter)
+incprob=apply(prob_mcmc[,-c(1:500)],1,mean)
 ya=as.vector(res[[10]])
 ya=matrix(ya,p,niter)
+xa=as.vector(res[[11]])
+xa=matrix(xa,p,p)
+scale=res[[12]]
+
 
 #Roda
 simdata = data.frame(xo,yo)
 burnin.sim <- 500;
-Gtot <- 10000;
+Gtot <- 20000;
 Sys.time()->start;
 oda.n <- oda.bma(x=simdata[,-dim(simdata)[2]],y=simdata$yo,niter=Gtot,burnin=burnin.sim,model="lm",prior="normal") # Normal prior for lm 
 print(Sys.time()-start);
@@ -78,7 +87,33 @@ lmargcalc.final <- function(gammanew,xo,yo,lam)
   #return(list(logmarg=logmarg,xbeta=xbeta));
   return((logmarg));
 }
-lml.all=apply(models,1,lmargcalc.final,xo,yo,rep(1,dim(xo)[2]))
+lmargcalc.my <- function(gammanew,xo,yo,lam)
+{
+  xo <- as.matrix(xo); yo <- as.vector(yo);
+  no <- nrow(xo);  p <- ncol(xo);
+  sqrtvar <- sqrt(apply(xo,2,var))*sqrt((no-1)/no);
+  xo <- scale(xo,center=T,scale=sqrtvar);
+  Ino=diag(no);
+  one=rep(1,no);
+  P1=matrix(1/no,no,no);
+  Lam=diag(lam);
+  ncolumn <- sum(gammanew == 1);
+  a=(no+1)/2
+  if (ncolumn == 0)
+  {
+  b=t(yo)%*%(Ino-P1)%*%yo;
+    logmarg = -(0.5*(no-1))*log(2*pi)+(lgamma(a)-a*log(b)) ;
+  } else
+  {
+    xog <- xo[,gammanew==1];
+    Lamg=Lam[gammanew==1,gammanew==1];
+  b=t(yo)%*%(Ino-P1-xog%*%solve(t(xog)%*%xog+Lamg)%*%t(xog))%*%yo;
+    logmarg = -(0.5*(no-1))*log(2*pi)+lgamma(a) -a*log(b)+0.5*log(det(as.matrix(Lamg))) -0.5*log(det(as.matrix(Lamg+t(xog)%*%xog)))   ;
+  }
+  #return(list(logmarg=logmarg,xbeta=xbeta));
+  return((logmarg));
+}
+lml.all=apply(models,1,lmargcalc.my,xo,yo,rep(1,dim(xo)[2]))
 results=cbind(lml.all, models)
 order=sort(results[,1],index=TRUE,decreasing=TRUE)
 results[order$ix,]
@@ -92,5 +127,17 @@ inclusionprob=rep(0,dim(postprob)[2]-1)
 for(i in 1:dim(postprob)[2]-1){
   inclusionprob[i]=sum(postprob[,i+1]*postprob[,1]);
 }
-enumerate_inc_prob=round(inclusionprob,3)
+enumerate_inc_prob=round(inclusionprob,6)
 enumerate_inc_prob
+
+
+
+par(mfrow=c(2,4))
+for(i in 1:p){
+plot(density(ya[i,]))
+}
+
+par(mfrow=c(2,4))
+for(i in 1:p){
+hist(prob_mcmc[i,])
+}
